308f9154eb45895eb1e4cfab6c94665c27c645c5
Alex Crear Videojuegos <alex@crearvideojuegos.com>
2024-07-03 18:25:09 +0200

    Actualizacion Cursos

---
+++ Cursos Originales/Prototype 2 B/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float _timeShoot;
+    private bool _canShoot = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && _canShoot)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            _canShoot = false;
+            _timeShoot = 0f;
+        }
+
+        if(!_canShoot)
+        {
+            if(_timeShoot > 3f)
+            {
+                _canShoot = true;
+            }
+        }
+        
+        _timeShoot += Time.deltaTime;
+    }
+}
