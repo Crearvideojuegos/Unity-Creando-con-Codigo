1cd88830bd40ce7120eac2b3b113438062a21662
Alex Crear Videojuegos <alex@crearvideojuegos.com>
2024-07-04 16:46:32 +0200

    Subida Prototype 2B

---
+++ Cursos Actualizados/Prototype 2B/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject DogPrefab;
+    private PlayerActions _playerActions; //Input System
+    private bool _canShoot;
+    private float _timeShoot;
+
+    // Update is called once per frame
+    private void Awake() 
+    {
+        _playerActions = new PlayerActions();
+    }
+
+    private void Start()
+    {
+        _playerActions.Enable();
+        _playerActions.PlayerController.Shoot.performed += ctx => Shoot();
+        _canShoot = true;
+    }
+
+    private void FixedUpdate() 
+    {
+        PlayerCanShoot();
+    }
+
+    private void Shoot()
+    {
+        if(_canShoot)
+        {
+            Instantiate(DogPrefab, transform.position, DogPrefab.transform.rotation);
+            _canShoot = false;
+            _timeShoot = 0f;
+        }
+    }
+
+    private void PlayerCanShoot()
+    {
+        if(!_canShoot)
+        {
+            if(_timeShoot > 3f)
+            {
+                _canShoot = true;
+            }
+        }
+        
+        _timeShoot += Time.deltaTime;
+    }
+
+}
---
+++ Cursos Actualizados/Prototype 2B/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] BallPrefabs;
+
+    private float _spawnLimitXLeft = -22;
+    private float _spawnLimitXRight = 7;
+    private float _spawnPosY = 30;
+
+    private float _startDelay = 1.0f;
+    private float _spawnIntervalOne = 4.0f;
+    private float _spawnIntervalTwo = 6.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", _startDelay, Random.Range(_spawnIntervalOne, _spawnIntervalTwo));
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(_spawnLimitXLeft, _spawnLimitXRight), _spawnPosY, 0);
+        int ballIndex = Random.Range(0, BallPrefabs.Length);
+
+        // instantiate ball at random spawn location
+        Instantiate(BallPrefabs[ballIndex], spawnPos, BallPrefabs[ballIndex].transform.rotation);
+    }
+
+}
